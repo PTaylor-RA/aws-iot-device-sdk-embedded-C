cf5f2d388ae793c46d7d77b94fbe6ab9e9e75675
PeterT <ptaylor@rayallen.com>
2016-12-20 11:08:03 -0700

    custom config; remove constant thing and client names;

--- src/aws_iot_shadow.c
+++ src/aws_iot_shadow.c
@@ -35,8 +35,8 @@ extern "C" {
 const ShadowInitParameters_t ShadowInitParametersDefault = {(char *) AWS_IOT_MQTT_HOST, AWS_IOT_MQTT_PORT, NULL, NULL,
 															NULL, false, NULL};
 
-const ShadowConnectParameters_t ShadowConnectParametersDefault = {(char *) AWS_IOT_MY_THING_NAME,
-																  (char *) AWS_IOT_MQTT_CLIENT_ID, 0};
+const ShadowConnectParameters_t ShadowConnectParametersDefault = {(char *) "",
+																  (char *) "", 0};
 
 void aws_iot_shadow_reset_last_received_version(void) {
 	shadowJsonVersionNum = 0;
